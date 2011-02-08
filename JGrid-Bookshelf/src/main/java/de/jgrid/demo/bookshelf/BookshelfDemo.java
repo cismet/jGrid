a7d280e77d82716bbcb732fdba2daac312dff043
Hendrik.Ebbers@web.de <Hendrik.Ebbers@web.de@17bb0ecf-38c4-95f9-1e6d-ca9926013565>
2011-02-08 19:43:58 +0000

    Progressbar for loading
    git-svn-id: http://jgrid.googlecode.com/svn/trunk@25 17bb0ecf-38c4-95f9-1e6d-ca9926013565

--- JGrid-Bookshelf/src/main/java/de/jgrid/demo/bookshelf/BookshelfDemo.java
+++ JGrid-Bookshelf/src/main/java/de/jgrid/demo/bookshelf/BookshelfDemo.java
@@ -14,15 +14,20 @@
  */
 package de.jgrid.demo.bookshelf;
 
-import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
-import javax.swing.ListModel;
-import javax.swing.event.ListDataListener;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
 import de.jgrid.JGrid;
+import de.jgrid.demo.util.CoolProgressBarUI;
 
 public class BookshelfDemo extends JFrame {
 
@@ -35,53 +40,98 @@ public class BookshelfDemo extends JFrame {
 		setTitle("BookshelfDemo");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		final List<Book> bookList = new ArrayList<Book>();
-		bookList.add(new Book("Unseen Academicals", "0552153370"));
-		bookList.add(new Book("Thief of time", "0552148407"));
-		bookList.add(new Book("The truth", "0552147680"));
-		bookList.add(new Book("The fifth elephant", "0552146161"));
-		bookList.add(new Book("Carpe Jugulum", "0552146153"));
-		bookList.add(new Book("Jingo", "055214598X"));
-		bookList.add(new Book("Feet of Clay", "0552142379"));
-		bookList.add(new Book("-", "0552142360"));
-		bookList.add(new Book("-", "0552138916"));
-		bookList.add(new Book("-", "0552146145"));
-		bookList.add(new Book("-", "0552140295"));
-		bookList.add(new Book("-", "0552134651"));
-		bookList.add(new Book("-", "0552134643"));
-		bookList.add(new Book("-", "0552134635"));
-		bookList.add(new Book("-", "1857989546"));
-		bookList.add(new Book("-", "0552134627"));
-		bookList.add(new Book("-", "1423101499"));
-		bookList.add(new Book("-", "0061477931"));
 		
-		JGrid grid = new JGrid(new ListModel() {
+		final JFrame loadFrame = new JFrame("Loading Demo");
+		loadFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final JProgressBar bar = new JProgressBar();
+		bar.setForeground(Color.ORANGE);
+		bar.setUI(new CoolProgressBarUI());
+		loadFrame.getContentPane().setLayout(
+				new FlowLayout(SwingUtilities.CENTER, 20, 20));
+		loadFrame.getContentPane().add(bar);
+		loadFrame.setResizable(false);
+		loadFrame.pack();
+		loadFrame.setLocationRelativeTo(null);
+		loadFrame.setBackground(Color.DARK_GRAY);
+		loadFrame.setVisible(true);
+		
+		final DefaultListModel model = new DefaultListModel();
+		
+		
+		SwingWorker<Void, Book> worker = new SwingWorker<Void, Book>() {
 
 			@Override
-			public void removeListDataListener(ListDataListener l) {
+			protected Void doInBackground() throws Exception {
+				int count = 18;
+				int i = 0;
+				setProgress((100 / count) * i++);
+				publish(new Book("Unseen Academicals", "0552153370"));
+				setProgress((100 / count) * i++);
+				publish(new Book("Thief of time", "0552148407"));
+				setProgress((100 / count) * i++);
+				publish(new Book("The truth", "0552147680"));
+				setProgress((100 / count) * i++);
+				publish(new Book("The fifth elephant", "0552146161"));
+				setProgress((100 / count) * i++);
+				publish(new Book("Carpe Jugulum", "0552146153"));
+				setProgress((100 / count) * i++);
+				publish(new Book("Jingo", "055214598X"));
+				setProgress((100 / count) * i++);
+				publish(new Book("Feet of Clay", "0552142379"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0552142360"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0552138916"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0552146145"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0552140295"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0552134651"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0552134643"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0552134635"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "1857989546"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0552134627"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "1423101499"));
+				setProgress((100 / count) * i++);
+				publish(new Book("-", "0061477931"));
+				return null;
 			}
 
 			@Override
-			public int getSize() {
-				return bookList.size();
+			protected void process(List<Book> chunks) {
+				bar.setValue(getProgress());
+				for (Iterator<Book> iterator = chunks.iterator(); iterator
+						.hasNext();) {
+					model.addElement(iterator.next());
+				}
 			}
 
 			@Override
-			public Object getElementAt(int index) {
-				return bookList.get(index);
+			protected void done() {
+				loadFrame.setVisible(false);
+				setVisible(true);
 			}
-
-			@Override
-			public void addListDataListener(ListDataListener l) {
-			}
-		});
+		};
+		worker.execute();
+		
+		
+		
+		
+		JGrid grid = new JGrid(model);
 		grid.setDefaultCellRenderer(new OpenLibraryGridRenderer());
 		grid.setUI(new BookshelfUI());
 		getContentPane().add(new JScrollPane(grid));
 		setSize(800, 600);
+		setLocationRelativeTo(null);
 	}
 	
 	public static void main(String[] args) {
-		new BookshelfDemo().setVisible(true);
+		new BookshelfDemo();
 	}
 }
--- JGrid-Pictureviewer/src/main/java/de/jgrid/demo/picviewer/PicViewerDemo.java
+++ JGrid-Pictureviewer/src/main/java/de/jgrid/demo/picviewer/PicViewerDemo.java
@@ -15,11 +15,14 @@
 package de.jgrid.demo.picviewer;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.DefaultListModel;
@@ -28,14 +31,17 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import javax.swing.UIManager;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import de.jgrid.JGrid;
+import de.jgrid.demo.util.CoolProgressBarUI;
 import de.jgrid.demo.util.UrlLoader;
 
 public class PicViewerDemo extends JFrame {
@@ -45,72 +51,78 @@ public class PicViewerDemo extends JFrame {
 	public PicViewerDemo() {
 		setTitle("PictureViewer");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		DefaultListModel model = new DefaultListModel();
-		for (int i = 1; i < 5; i++) {
-			try {
-				model.addElement(new PicViewerObject("/de/jgrid/demo/picviewer/coast/", (float) i / 5.0f));
-				model.addElement(new PicViewerObject("/de/jgrid/demo/picviewer/dogs/", (float) i / 5.0f));
-				model.addElement(new PicViewerObject("/de/jgrid/demo/picviewer/emotions/", (float) i / 5.0f));
-				model.addElement(new PicViewerObject("/de/jgrid/demo/picviewer/flowers/", (float) i / 5.0f));
-				model.addElement(new PicViewerObject("/de/jgrid/demo/picviewer/landscape/", (float) i / 5.0f));
-			} catch (Exception exception) {
-				exception.printStackTrace();
-			}
-		}
-		
+
+		final DefaultListModel model = new DefaultListModel();
+
+		final JFrame loadFrame = new JFrame("Loading Demo");
+		loadFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final JProgressBar bar = new JProgressBar();
+		bar.setForeground(Color.ORANGE);
+		bar.setUI(new CoolProgressBarUI());
+		loadFrame.getContentPane().setLayout(
+				new FlowLayout(SwingUtilities.CENTER, 20, 20));
+		loadFrame.getContentPane().add(bar);
+		loadFrame.setResizable(false);
+		loadFrame.pack();
+		loadFrame.setLocationRelativeTo(null);
+		loadFrame.setBackground(Color.DARK_GRAY);
+		loadFrame.setVisible(true);
+
 		final JGrid grid = new JGrid(model);
 		grid.setDefaultCellRenderer(new PicViewerRenderer());
 		grid.setFixedCellDimension(160);
 		grid.addMouseMotionListener(new MouseAdapter() {
-			
+
 			int lastIndex = -1;
-			
+
 			@Override
 			public void mouseMoved(MouseEvent e) {
-				if(lastIndex >= 0) {
+				if (lastIndex >= 0) {
 					Object o = grid.getModel().getElementAt(lastIndex);
-					if(o instanceof PicViewerObject) {
+					if (o instanceof PicViewerObject) {
 						Rectangle r = grid.getCellBounds(lastIndex);
-						if(r != null && !r.contains(e.getPoint())) {
+						if (r != null && !r.contains(e.getPoint())) {
 							((PicViewerObject) o).setMarker(false);
 							grid.repaint(r);
 						}
 					}
 				}
-				
+
 				int index = grid.getCellAt(e.getPoint());
-				if(index >= 0) {
+				if (index >= 0) {
 					Object o = grid.getModel().getElementAt(index);
-					if(o instanceof PicViewerObject) {
+					if (o instanceof PicViewerObject) {
 						Rectangle r = grid.getCellBounds(index);
-						if(r != null) {
-							((PicViewerObject) o).setFraction(((float)e.getPoint().x - (float)r.x) / (float)r.width);
+						if (r != null) {
+							((PicViewerObject) o).setFraction(((float) e
+									.getPoint().x - (float) r.x)
+									/ (float) r.width);
 							((PicViewerObject) o).setMarker(true);
 							lastIndex = index;
 							grid.repaint(r);
 						}
 					}
 				}
-				
+
 			}
 		});
-		
+
 		JScrollPane scrollPane = new JScrollPane(grid);
 		scrollPane.setBorder(null);
-		
+
 		getContentPane().add(scrollPane, BorderLayout.CENTER);
-		
+
 		JPanel controlPanel = new JPanel();
 		controlPanel.setLayout(new BorderLayout());
 		JButton creditsButton = new JButton("image credits");
 		controlPanel.add(creditsButton, BorderLayout.WEST);
-		
-		final JSlider slider = new JSlider(128, 256, grid.getFixedCellDimension());
+
+		final JSlider slider = new JSlider(128, 256, grid
+				.getFixedCellDimension());
 		slider.setValue(grid.getFixedCellDimension());
-		slider.putClientProperty( "JComponent.sizeVariant", "small" );
+		slider.putClientProperty("JComponent.sizeVariant", "small");
 		slider.addChangeListener(new ChangeListener() {
-			
+
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				grid.setFixedCellDimension(slider.getValue());
@@ -119,13 +131,17 @@ public class PicViewerDemo extends JFrame {
 		JPanel sliderPanel = new JPanel();
 		sliderPanel.setLayout(new FlowLayout());
 		try {
-			sliderPanel.add(new JLabel(new ImageIcon(ImageIO.read(UrlLoader.getInstance().load("/de/jgrid/demo/picviewer/dim_small.png")))));
+			sliderPanel.add(new JLabel(new ImageIcon(ImageIO.read(UrlLoader
+					.getInstance().load(
+							"/de/jgrid/demo/picviewer/dim_small.png")))));
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		sliderPanel.add(slider);
 		try {
-			sliderPanel.add(new JLabel(new ImageIcon(ImageIO.read(UrlLoader.getInstance().load("/de/jgrid/demo/picviewer/dim_large.png")))));
+			sliderPanel.add(new JLabel(new ImageIcon(ImageIO.read(UrlLoader
+					.getInstance().load(
+							"/de/jgrid/demo/picviewer/dim_large.png")))));
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -133,8 +149,60 @@ public class PicViewerDemo extends JFrame {
 		getContentPane().add(controlPanel, BorderLayout.SOUTH);
 		getContentPane().setBackground(grid.getBackground());
 		setSize(800, 600);
+		setLocationRelativeTo(null);
+
+		SwingWorker<Void, PicViewerObject> worker = new SwingWorker<Void, PicViewerObject>() {
+
+			@Override
+			protected Void doInBackground() throws Exception {
+				int repeatCount = 5;
+				for (int i = 1; i <= repeatCount; i++) {
+					try {
+						setProgress((i - 1) * 100 / repeatCount + 4);
+						publish(new PicViewerObject(
+								"/de/jgrid/demo/picviewer/coast/",
+								(float) i / 5.0f));
+						setProgress((i - 1) * 100 / repeatCount + 8);
+						publish(new PicViewerObject(
+								"/de/jgrid/demo/picviewer/dogs/",
+								(float) i / 5.0f));
+						setProgress((i - 1) * 100 / repeatCount + 12);
+						publish(new PicViewerObject(
+								"/de/jgrid/demo/picviewer/emotions/",
+								(float) i / 5.0f));
+						setProgress((i - 1) * 100 / repeatCount + 16);
+						publish(new PicViewerObject(
+								"/de/jgrid/demo/picviewer/flowers/",
+								(float) i / 5.0f));
+						setProgress((i - 1) * 100 / repeatCount + 20);
+						publish(new PicViewerObject(
+								"/de/jgrid/demo/picviewer/landscape/",
+								(float) i / 5.0f));
+					} catch (Exception exception) {
+						exception.printStackTrace();
+					}
+				}
+				return null;
+			}
+
+			@Override
+			protected void process(List<PicViewerObject> chunks) {
+				bar.setValue(getProgress());
+				for (Iterator<PicViewerObject> iterator = chunks.iterator(); iterator
+						.hasNext();) {
+					model.addElement(iterator.next());
+				}
+			}
+
+			@Override
+			protected void done() {
+				loadFrame.setVisible(false);
+				setVisible(true);
+			}
+		};
+		worker.execute();
 	}
-	
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 
@@ -146,7 +214,7 @@ public class PicViewerDemo extends JFrame {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				new PicViewerDemo().setVisible(true);
+				new PicViewerDemo();
 			}
 		});
 	}
